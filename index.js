025c0d814ebae3389076dede5c9d0a5324719a32
Andyeeeee <10514f074@gmail.com>
2024-03-03 22:52:48 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var y = 10;
+var x = 11;
+x = "hellow-word";
+var c = "hellow-word";
+c = "new-word";
+var arry = [5, 6, 7];
+arry.push('hey');
+var q;
+q = [false, true];
+console.log(x, y, c, arry, q);
---
+++ index.ts
@@ -0,0 +1,13 @@
+const y: number = 10;
+const x: number = 11
+x = "hellow-word"
+let c: string = "hellow-word"
+c = "new-word"
+
+let arry: number[] = [5, 6, 7]
+arry.push('hey')
+
+let q: barry2<boolean>;
+q = [false, true]
+
+console.log(x, y, c, arry, q);
